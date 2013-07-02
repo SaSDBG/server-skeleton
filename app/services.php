5cd8a3455384d79fa107cad80f698e0ac6e331c3
drak3 <drak3@live.de>
2013-07-02 16:13:21 +0200

    fixed LoggerService

--- app/services.php
+++ app/services.php
@@ -4,7 +4,7 @@ use SaS\ServiceProvider\APIServiceProvider;
 use SaS\ServiceProvider\LoggerServiceProvider;
 use SaS\ServiceProvider\DoctrineProvider;
 
-$app->register(new LoggerServiceProvider());
+$app->register(new LoggerProvider());
 $app->register(new APIServiceProvider());
 $app->register(new DoctrineProvider())
 
